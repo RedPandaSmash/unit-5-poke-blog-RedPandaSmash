6403b4d9364b780d2d2d477707e9f94732f8869a
RedPandaSmash <redpandasmash@gmail.com>
2025-06-30 20:14:25 -0400

    stuck on search

--- index.js
+++ index.js
@@ -22,22 +22,97 @@ app.get("/", async (req, res) => {
 });
 
 //get all pokemon in the pokedex
-app.get("/pokemon");
+app.get("/pokemon", async (req, res) => {
+  try {
+    res.json(pokedex);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      res.json([]);
+    } else {
+      res.status(500).json({ error: "Failed to retreive pokemon" });
+    }
+  }
+});
 
 //get pokemon by dex number
 app.get("/pokemon/:id", async (req, res) => {
   try {
     const PKMNID = parseInt(req.params.id);
     //validation check if id is not greater than 0 or exceeds array length
-    if (PKMNID < 0 || PKMNID > pokedex.length) {
-      error(`A pokemon of with that ID has not been documented yet.`);
+    if (PKMNID <= 0 || PKMNID > pokedex.length) {
+      res.json({
+        error: `A pokemon with that ID has not been documented yet.`,
+      });
     }
-    res.json(pokedex.indexof[PKMNID - 1]);
-  } catch {}
+    res.json(pokedex[PKMNID - 1]);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      res
+        .status(404)
+        .json({ error: "A pokemon with the ID has not been documented yet" });
+    } else {
+      res.status(500).json({ error: "Failed to retreive pokemon" });
+    }
+  }
 });
 
 //get all matching pokemon
-app.get("/search");
+app.get("/search", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 //add a new pokemon to the dex
-app.post("/pokemon");
+app.post("/pokemon", async (req, res) => {
+  try {
+    const {
+      name,
+      hitPoints,
+      attack,
+      defense,
+      description,
+      abilities,
+      evolution,
+      image,
+      id,
+    } = req.body;
+    if (
+      !name ||
+      !hitPoints ||
+      !attack ||
+      !defense ||
+      !description ||
+      !abilities ||
+      !evolution ||
+      !image ||
+      !id
+    ) {
+      return res.status(422).json({
+        error:
+          "All fields must be entered. If not applicable, enter a blank object or string!",
+      });
+    }
+    const newPKMN = {
+      name,
+      hitPoints,
+      attack,
+      defense,
+      description,
+      abilities,
+      evolution,
+      image,
+      id,
+    };
+
+    pokedex.push(newPKMN);
+
+    res.status(201).json({
+      message: "pokemon added!",
+      newentry: newPKMN,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "something went wrong trying to add a 'mon." });
+  }
+});
